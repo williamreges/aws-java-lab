f56182d4f9e6b867b95b0f23f00f8ee8bc9cd758
William Reges <william.reges1986@gmail.com>
2024-10-31 15:34:27 -0300

    ajuste

--- lambdas/validadigitocpffunction/02_maven_package.sh
+++ lambdas/validadigitocpffunction/02_maven_package.sh
@@ -3,5 +3,5 @@ echo "=== Criando pacote Maven ==="
 mvn  clean install
 
 echo "=== Gerando ZIP lambdafunction.zip"
-smv ./target/aws-lambda-validadigitocpffunction-1.0-SNAPSHOT.jar ./app.jar
+mv ./target/aws-lambda-validadigitocpffunction-1.0-SNAPSHOT.jar ./app.jar
 echo "=== Zip Gerado ==="
